abe5ea57bd22a069790ccd761ee62e0e5a0423bf
Marconi Jiang <marconi1964@yahoo.com>
2021-01-17 08:16:37 +0800

    update

--- darknet.sh
+++ darknet.sh
@@ -3,6 +3,8 @@
 # prerequite was prepared in "darknet_setup.sh"
 # execution in "darknet.sh"
 
+# check README_CUDA_installation.md and README_training_on_server.md
+
 # Download tag'ed image files from 
 cd ~
 wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1yf4clDi07G_DMF0Jfh9ZZVp-E7L3KFkJ' -O Object_11_20.rar
